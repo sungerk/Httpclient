e641d09ec87ff27eca8e8c3b82925bbc6eff2b3a
sungerk <sungerk@qq.com>
2015-09-12 20:43:20 +0800

    修复已知bug

--- library/src/main/java/org/sunger/lib/http/asyn/AsyncHttpResponseHandler.java
+++ library/src/main/java/org/sunger/lib/http/asyn/AsyncHttpResponseHandler.java
@@ -7,62 +7,63 @@ import android.os.Looper;
 import org.sunger.lib.http.client.HttpResponse;
 
 public abstract class AsyncHttpResponseHandler implements
-		ResponseHandlerInterface {
-	protected static final int SUCCESS_MESSAGE = 0;
-	protected static final int FAILURE_MESSAGE = 1;
-	protected static final int START_MESSAGE = 2;
+        ResponseHandlerInterface {
+    protected static final int SUCCESS_MESSAGE = 0;
+    protected static final int FAILURE_MESSAGE = 1;
+    protected static final int START_MESSAGE = 2;
 
-	private Handler mDelivery;
+    private Handler mDelivery;
 
-	public abstract void onSuccess(HttpResponse response);
+    public abstract void onSuccess(int stateCode, String data);
 
-	public void onFailure(Throwable error) {
-	}
+    public void onFailure(Throwable error) {
+    }
 
-	protected void onStart() {
+    protected void onStart() {
 
-	}
+    }
 
-	public AsyncHttpResponseHandler() {
-		mDelivery = new Handler(Looper.getMainLooper());
-	}
+    public AsyncHttpResponseHandler() {
+        mDelivery = new Handler(Looper.getMainLooper());
+    }
 
-	@Override
-	public void sendStartMessage() {
-		sendMessage(START_MESSAGE, null);
-	}
+    @Override
+    public void sendStartMessage() {
+        sendMessage(START_MESSAGE, null);
+    }
 
-	@Override
-	public void sendFailureMessage(Throwable error) {
-		sendMessage(FAILURE_MESSAGE, error);
-	}
+    @Override
+    public void sendFailureMessage(Throwable error) {
+        sendMessage(FAILURE_MESSAGE, error);
+    }
 
-	@Override
-	public void sendSuccessMessage(HttpResponse response) {
-		sendMessage(SUCCESS_MESSAGE, response);
-	}
+    @Override
+    public void sendSuccessMessage(HttpResponse response) {
+        sendMessage(SUCCESS_MESSAGE, response);
+    }
 
-	private void handlerMessage(int type, Object msg) {
-		switch (type) {
-		case SUCCESS_MESSAGE:
-			onSuccess((HttpResponse) msg);
-			break;
-		case FAILURE_MESSAGE:
-			onFailure((Throwable) msg);
-			break;
-		case START_MESSAGE:
-			onStart();
-			break;
-		}
-	}
+    private void handlerMessage(int type, Object msg) {
+        switch (type) {
+            case SUCCESS_MESSAGE:
+                HttpResponse response=(HttpResponse) msg;
+                onSuccess(response.getStatusCode(),response.getResult());
+                break;
+            case FAILURE_MESSAGE:
+                onFailure((Throwable) msg);
+                break;
+            case START_MESSAGE:
+                onStart();
+                break;
+        }
+    }
 
-	protected void sendMessage(final int type, final Object msg) {
-		mDelivery.post(new Runnable() {
-			@Override
-			public void run() {
-				handlerMessage(type, msg);
-			}
-		});
-	}
+    protected void sendMessage(final int type, final Object msg) {
+        mDelivery.post(new Runnable() {
+            @Override
+            public void run() {
+                handlerMessage(type, msg);
+            }
+        });
+    }
 
 }
--- library/src/main/java/org/sunger/lib/http/client/HttpResponse.java
+++ library/src/main/java/org/sunger/lib/http/client/HttpResponse.java
@@ -14,115 +14,127 @@ import java.util.List;
 import java.util.Map;
 
 public final class HttpResponse {
-	private final int statusCode;
-	private final Map<String, List<String>> headers;
-	private InputStream payload;
-	private int contentLength;
-	private String fileName;
+    private final int statusCode;
+    private final Map<String, List<String>> headers;
+    private InputStream payload;
+    private int contentLength;
+    private String fileName;
+    private String result;
 
-	HttpResponse(final int statusCode, int contentLength, String fileName,
-			final InputStream payload,
-			final Map<String, List<String>> rawHeaders) {
-		this.statusCode = statusCode;
-		this.payload = payload;
-		this.fileName = fileName;
-		this.contentLength = contentLength;
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD) {
-			final Map<String, List<String>> newHeaders = new HashMap<String, List<String>>(
-					rawHeaders.size());
-			for (final Map.Entry<String, List<String>> e : rawHeaders
-					.entrySet()) {
-				final String key = e.getKey();
-				final int keyLen = key.length();
-				final StringBuilder newKey = new StringBuilder(keyLen);
-				for (int i = 0; i < keyLen; ++i) {
-					final char c = key.charAt(i);
-					final char c2;
-					if (i == 0 || key.charAt(i - 1) == '-') {
-						c2 = Character.toUpperCase(c);
-					} else {
-						c2 = c;
-					}
-					newKey.append(c2);
-				}
-				newHeaders.put(newKey.toString(), e.getValue());
-			}
-			this.headers = Collections.unmodifiableMap(newHeaders);
-		} else {
-			this.headers = Collections.unmodifiableMap(rawHeaders);
-		}
-	}
+    HttpResponse(final int statusCode, int contentLength, String fileName,
+                 final InputStream payload,
+                 final Map<String, List<String>> rawHeaders) {
+        this.statusCode = statusCode;
+        this.payload = payload;
+        this.fileName = fileName;
+        this.contentLength = contentLength;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD) {
+            final Map<String, List<String>> newHeaders = new HashMap<String, List<String>>(
+                    rawHeaders.size());
+            for (final Map.Entry<String, List<String>> e : rawHeaders
+                    .entrySet()) {
+                final String key = e.getKey();
+                final int keyLen = key.length();
+                final StringBuilder newKey = new StringBuilder(keyLen);
+                for (int i = 0; i < keyLen; ++i) {
+                    final char c = key.charAt(i);
+                    final char c2;
+                    if (i == 0 || key.charAt(i - 1) == '-') {
+                        c2 = Character.toUpperCase(c);
+                    } else {
+                        c2 = c;
+                    }
+                    newKey.append(c2);
+                }
+                newHeaders.put(newKey.toString(), e.getValue());
+            }
+            this.headers = Collections.unmodifiableMap(newHeaders);
+        } else {
+            this.headers = Collections.unmodifiableMap(rawHeaders);
+        }
+        this.result = readToText();
+    }
+    private String readToText() {
+        StringBuilder buffer = new StringBuilder();
+        try {
+            String enc = getContentCharset();
+            if (enc == null) {
+                enc = "UTF-8";
+            }
+            InputStream input = getPayload();
+            InputStreamReader reader = new InputStreamReader(input, enc);
+            final char[] inBuf = new char[64];
+            for (int charsRead; (charsRead = reader.read(inBuf)) != -1; ) {
+                buffer.append(inBuf, 0, charsRead);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return buffer.toString();
 
-	public String getContentType() {
-		final String contentType = getFirstHeaderValue("Content-Type");
-		if (contentType == null) {
-			return null;
-		}
+    }
 
-		final int i = contentType.indexOf(';');
-		return i == -1 ? contentType : contentType.substring(0, i).trim();
-	}
+    public String getContentType() {
+        final String contentType = getFirstHeaderValue("Content-Type");
+        if (contentType == null) {
+            return null;
+        }
 
-	public String getContentCharset() {
-		final String contentType = getFirstHeaderValue("Content-Type");
-		if (contentType == null) {
-			return null;
-		}
-		final int i = contentType.indexOf('=');
-		return i == -1 ? null : contentType.substring(i + 1).trim();
-	}
+        final int i = contentType.indexOf(';');
+        return i == -1 ? contentType : contentType.substring(0, i).trim();
+    }
 
-	public InputStream getPayload() {
-		return payload;
-	}
+    public String getContentCharset() {
+        final String contentType = getFirstHeaderValue("Content-Type");
+        if (contentType == null) {
+            return null;
+        }
+        final int i = contentType.indexOf('=');
+        return i == -1 ? null : contentType.substring(i + 1).trim();
+    }
 
-	void preload(File temp) throws IOException {
-		@SuppressWarnings("resource")
-		final FileOutputStream out = new FileOutputStream(temp);
-		final byte[] inBuf = new byte[1024];
-		final InputStream in = getPayload();
-		for (int bytesRead = 0; (bytesRead = in.read(inBuf)) != -1;) {
-			out.write(inBuf, 0, bytesRead);
-		}
+    public InputStream getPayload() {
+        return payload;
+    }
 
-		payload = new FileInputStream(temp);
-	}
+    void preload(File temp) throws IOException {
+        @SuppressWarnings("resource")
+        final FileOutputStream out = new FileOutputStream(temp);
+        final byte[] inBuf = new byte[1024];
+        final InputStream in = getPayload();
+        for (int bytesRead = 0; (bytesRead = in.read(inBuf)) != -1; ) {
+            out.write(inBuf, 0, bytesRead);
+        }
 
-	public void read(StringBuilder buffer) throws IOException {
-		String enc = getContentCharset();
-		if (enc == null) {
-			enc = "UTF-8";
-		}
+        payload = new FileInputStream(temp);
+    }
 
-		final InputStream input = getPayload();
-		final InputStreamReader reader = new InputStreamReader(input, enc);
-		final char[] inBuf = new char[64];
-		for (int charsRead; (charsRead = reader.read(inBuf)) != -1;) {
-			buffer.append(inBuf, 0, charsRead);
-		}
-	}
 
-	public int getStatusCode() {
-		return statusCode;
-	}
 
-	public Map<String, List<String>> getHeaders() {
-		return headers;
-	}
+    public int getStatusCode() {
+        return statusCode;
+    }
 
-	public String getFirstHeaderValue(String name) {
-		final List<String> values = headers.get(name);
-		if (values == null || values.isEmpty()) {
-			return null;
-		}
-		return values.get(0);
-	}
+    public Map<String, List<String>> getHeaders() {
+        return headers;
+    }
 
-	public int getContentLength() {
-		return contentLength;
-	}
+    public String getFirstHeaderValue(String name) {
+        final List<String> values = headers.get(name);
+        if (values == null || values.isEmpty()) {
+            return null;
+        }
+        return values.get(0);
+    }
 
-	public String getFileName() {
-		return fileName;
-	}
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+    public String getResult() {
+        return result;
+    }
 }
